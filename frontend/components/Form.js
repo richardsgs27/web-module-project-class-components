59832efa117969bbe8309193b51f4dbe9c614534
Gigi <gigi@iMacs-iMac.local>
2022-03-11 15:02:31 -0600

    Done

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
-export default class Form extends React.Component {
-  render() {
-    return (
-      <div>
-        Form
-      </div>
-    )
+
+
+export default class TodoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    }
   }
+handleSubmit = evt => {
+  evt.preventDefault()
+  this.props.handleAdd(this.state.input);  
 }
+
+handleChange = evt => {
+  this.setState({
+    ...this.state,
+    input: evt.target.value
+  })
+}
+  render(){
+    return ( 
+      <form onClick={this.handleSubmit}>
+          <input onChange = {this.handleChange}/>
+          <button>Add</button>
+        </form>
+    )
+    }
+  }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export default class Todo extends React.Component {
+
+  handleClick = () => {
+   this.props.handleToggle(this.props.todo.id);
+  }
   render() {
-    return (
-      <div>
-        Todo
-      </div>
-    )
+    return (<li onClick={this.handleClick} key={this.props.todo.id}>{this.props.todo.name} {this.props.todo.completed?<span> ✔ </span> : <span>✖ </span>}</li>)
   }
 }
