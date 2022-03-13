973b0e04a4e16e6f17bf9c37b1c1ce7974328c2e
Gigi <gigi@iMacs-iMac.local>
2022-03-13 16:05:59 -0500

    Workin...

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,51 @@
 import React from 'react'
 
+const todos = [
+  {
+    name: 'Organize Garage 🏚 ', 
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    name: 'Bake Cookies 🍪 ', 
+    id: 1528817084358,
+    completed: false
+  },
+  {
+    name: 'Bake Chicken 🐔 ', 
+    id: 1528817084359,
+    completed: false
+  }
+]
+
+const initialState = {
+  errorMessage: '',
+  todos,
+  form: {
+    name: '',
+    completed: false
+  }
+}
+
 export default class App extends React.Component {
+  state = initialState
+
   render() {
+    console.log('props are', this.props)
+    console.log('state is', this.state)
+
+    const {todos} = this.state
+   
     return (
       <div>
-        Todo App
+       <h2>Todo App</h2>
+       <ul>
+         {
+           todos.map((todo) => {
+             return <li className='td' key={todo.id}>{todo.name}</li>
+           })
+         }
+       </ul>
       </div>
     )
   }
